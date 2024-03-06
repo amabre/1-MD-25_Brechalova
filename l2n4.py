55a1449a70d3f5490dbf37886b66b5017ae83bae
amabre <raydexamorte@gmail.com>
2024-03-06 12:50:29 +0300

    /ti6y

--- l2n4.py
+++ l2n4.py
@@ -1,10 +1,14 @@
 import random
+import math
 err=0
 pr=0
 while err!=3:
-    x1=random.randint(1,10)
-    x2=random.randint(1, 10)
-    ps=int(input(f"{x1}+{x2}="))
+    x1=random.randint(-10,10)
+    x2=random.randint(-10, 10)
+    if x2/math.fabs(x2)==-1:
+        ps = int(input(f"{x1}-{int(math.fabs(x2))}="))
+    else:
+        ps=int(input(f"{x1}+{x2}="))
     if ps==x1+x2:
         pr=pr+1
         print(":)")
